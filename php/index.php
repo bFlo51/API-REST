239783ee591fe0a3c375991ad02184b887093add
unknown <ludovit.martonyik@gmail.com>
2018-11-16 16:09:06 +0100

    Debug Ludo

--- php/index.php
+++ php/index.php
@@ -1,6 +1,11 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+$root = __DIR__ . '/..';
+$autoload = "$root/vendor/autoload.php";
+error_log ("loading autoload : $autoload");
+require $autoload;
+
+
 include 'config.php';
 $app = new Slim\App(["settings" => $config]);
 //Handle Dependencies
@@ -15,10 +20,30 @@ $container['db'] = function ($c) {
        );
        $pdo = new PDO("mysql:host=" . $db['servername'] . ";dbname=" . $db['dbname'],
        $db['username'], $db['password'],$options);
+       error_log("CONNECTED YEY ;) ");
        return $pdo;
    }
    catch(\Exception $ex){
+        error_log("CONNECTION KO :( ");
        return $ex->getMessage();
    }
    
 };
+
+
+
+try{
+    $db = $c['settings']['db'];
+    $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE                      => PDO::FETCH_ASSOC,
+    );
+    $pdo = new PDO("mysql:host=" . $db['servername'] . ";dbname=" . $db['dbname'],
+    $db['username'], $db['password'],$options);
+    error_log("CONNECTED YEY ;) ");
+    return $pdo;
+}
+catch(\Exception $ex){
+     error_log("CONNECTION KO :( ");
+     error_log($ex->getMessage());
+    return $ex->getMessage();
+}
